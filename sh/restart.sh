0e32b80522dae0d7c73f2021046f4753a368a201
zhangyang <1105626801@qq.com>
2021-09-29 17:33:59 +0800

    add

--- sh/restart.sh
+++ sh/restart.sh
@@ -2,14 +2,6 @@
 
 docker login --username lend -p $DOCKER_ACCESS_TOKEN
 
-docker stop spapi  spweb  sptask spinfo
-docker rm -f spapi  spweb  sptask spinfo
-
-docker rmi -f coinflow/bsc-sp-swap-web:latest  
-docker rmi -f  coinflow/bsc-sp-web-api:latest
-docker rmi -f coinflow/sp-info:latest
-docker rmi -f  coinflow/bsc-sp-web-task:latest
-docker-compose  -f  docker-compose.yml  pull spapi spweb sptask spinfo
 
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml up -d
